a91da6a71094a7f54c4cf29fe5981692b4d0dd94
TrentonMay <TrentonMay@fullsail.edu>
2014-12-03 18:44:08 -0500

    Created new prompt
    var depth = prompt ("Please enter the depth of the pool");

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -4,3 +4,4 @@
 alert ("We are calculating the amount of water need in your pool");
 var length = prompt ("Please enter the length of the pool");
 var width = prompt ("Please enter the width of the pool");
+var depth = prompt ("Please enter the depth of the pool");
