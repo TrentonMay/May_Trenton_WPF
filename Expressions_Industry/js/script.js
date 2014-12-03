4edee68871df115207d69b2bbe0c49921fd69019
TrentonMay <TrentonMay@fullsail.edu>
2014-12-03 18:37:47 -0500

    Changed comment

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -1,5 +1,5 @@
 //Expressions Industry
-//How many scan points on a foot
+//How many scan points on a foot patrol
 
 alert ("We are calculating the amount of scan points you will scan");//Tells user what we are doing
 var width = prompt ("Enter the width of the building");//Tells user to enter width of the building
