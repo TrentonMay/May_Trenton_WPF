2ced5b55601132c2c0d9dd8ad9988cc471b78507
TrentonMay <TrentonMay@fullsail.edu>
2014-12-03 18:11:26 -0500

    Created new prompt
    var freq = prompt ("Enter how often there is a scan point \n in feet");

--- Expressions_Industry/sctript.js
+++ Expressions_Industry/sctript.js
@@ -4,3 +4,8 @@
 alert ("We are calculating the amount of scan points you will scan");
 var width = prompt ("Enter the width of the building");
 var length = prompt ('Enter the length of the building');
+var perimeter = width * 2 + length * 2;
+var freq = prompt ("Enter how often there is a scan point \n in feet");
+
+
+
