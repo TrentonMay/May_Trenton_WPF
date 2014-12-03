18563676827db6a96767734057812f0d189ba33c
TrentonMay <TrentonMay@fullsail.edu>
2014-12-03 17:15:15 -0500

    Added a comment
    This will let us know whats happening in the script

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -1,7 +1,7 @@
 //Expressions Personal
 //How much carpet is needed in the living room
 
-alert("We are calculating the amount of carpet needed in the living room");
+alert("We are calculating the amount of carpet needed in the living room");//We are telling the user what we are doing
 var width = prompt("Enter the width of the living room");
 var length = prompt("Enter the length of the living room");
 var area = length * width;
