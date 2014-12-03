bb6195fc8d64d55bdf5a7908af46a07c0cb41d73
TrentonMay <TrentonMay@fullsail.edu>
2014-12-03 18:53:31 -0500

    Made sure user knows it's in feet

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -4,9 +4,9 @@
 alert ("We are calculating the amount of water need in your square pool \n Please enter in feet");
 
 
-var length = prompt ("Please enter the length of the pool");
-var width = prompt ("Please enter the width of the pool");
-var depth = prompt ("Please enter the depth of the pool");
+var length = prompt ("Please enter the length of the pool in feet");
+var width = prompt ("Please enter the width of the pool in feet");
+var depth = prompt ("Please enter the depth of the pool in feet");
 var volume = length * width * depth;
 var totalGal = volume * 7.47;
 
