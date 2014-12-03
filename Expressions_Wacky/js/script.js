1b9a01ae8996ea0eba97ab7e13e8cbee3dafde1a
TrentonMay <TrentonMay@fullsail.edu>
2014-12-03 18:47:55 -0500

    Edited alert

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -1,7 +1,9 @@
 //Expressions Wacky
 //We are calculating the amount of water needed in a pool
 
-alert ("We are calculating the amount of water need in your pool");
+alert ("We are calculating the amount of water need in your pool \n Please enter in feet");
+
+
 var length = prompt ("Please enter the length of the pool");
 var width = prompt ("Please enter the width of the pool");
 var depth = prompt ("Please enter the depth of the pool");
