a398b2ef4336d24a4840314ba9455145d849ecd1
TrentonMay <TrentonMay@fullsail.edu>
2014-12-03 18:20:01 -0500

    Fixed a typo in a prompt

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -5,7 +5,7 @@ alert ("We are calculating the amount of scan points you will scan");
 var width = prompt ("Enter the width of the building");
 var length = prompt ('Enter the length of the building');
 var perimeter = width * 2 + length * 2;
-var freq = prompt ("Enter how often there is a scan point \n in feet");
+var freq = prompt ("Enter how often there is a scan points \n in feet");
 var totalScan = perimeter / freq;
 
 alert ("The number of scan point you will have is " + Math.floor(totalScan));
