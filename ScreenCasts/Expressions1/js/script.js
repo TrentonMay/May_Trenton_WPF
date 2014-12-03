9619fa589a53e9c8c5079f978693b709c0ebcd7d
TrentonMay <TrentonMay@fullsail.edu>
2014-12-03 03:06:35 -0500

    summed up the quizzes

--- ScreenCasts/Expressions1/js/script.js
+++ ScreenCasts/Expressions1/js/script.js
@@ -5,3 +5,7 @@ var quiz2 = 85;
 var quiz3 = 70;
 var quiz4 = 89;
 
+//the average is the sum of the items divided by the number of items
+var average = quiz1 + quiz2 + quiz3 + quiz4;
+console.log(average);
+
