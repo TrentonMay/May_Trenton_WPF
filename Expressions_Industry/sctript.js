7eed13a4f07dc7a2c2aafbafd1e6316be3d3938a
TrentonMay <TrentonMay@fullsail.edu>
2014-12-03 18:08:41 -0500

    Created new alert
    alert ("We are calculating the amount of scan points you will scan");

--- Expressions_Industry/sctript.js
+++ Expressions_Industry/sctript.js
@@ -1,5 +1,6 @@
 //Expressions Industry
 //How many scan points on a foot patrol
 
+alert ("We are calculating the amount of scan points you will scan");
 var width = prompt ("Enter the width of the building");
 var length = prompt ('Enter the length of the building');
