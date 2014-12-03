3d10dd4889194a02c6df612d3ec90ac1e45dce27
TrentonMay <TrentonMay@fullsail.edu>
2014-12-03 17:07:21 -0500

    Started Expression
    Calculating how much carpet is needed in the living room

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -1,3 +1,5 @@
-/**
- * Created by Trent on 12/3/14.
- */
+//Expressions Personal
+//How much carpet is needed in the living room
+
+alert("We are calculating the amount of carpet needed in the living room");
+var width = prompt("Enter the width of the living room");
