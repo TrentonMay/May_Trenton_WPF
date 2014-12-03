d3ab0a2dcde114d26d99b2b32b04c1c16a69de37
TrentonMay <TrentonMay@fullsail.edu>
2014-12-03 17:34:04 -0500

    Added new variable and comment

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -3,7 +3,8 @@
 
 alert("We are calculating the amount of gas you will need");//We are telling the user what we are doing
 var ppg = prompt("Enter the price per gallon");//Tells user to enter the  price per gallon
-var mpg = prompt("Enter the miles per gallon your vehicle gets");//Tells customer to enter their mpg
+var mpg = prompt("Enter the miles per gallon your vehicle gets");//Tells user to enter their mpg
+var miles = prompt("Enter the amount of miles you will travel");//Tells user to enter how many miles they wish to travel
 var area = length * width;
 
 alert("The amount of carpet you need is " + area + " square ft");
