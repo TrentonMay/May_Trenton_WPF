3e2e259c7e161da4f14f53e06c76d97734f3259f
TrentonMay <TrentonMay@fullsail.edu>
2014-12-03 17:10:29 -0500

    Added var length

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -3,3 +3,7 @@
 
 alert("We are calculating the amount of carpet needed in the living room");
 var width = prompt("Enter the width of the living room");
+var length = prompt("Enter the length of the living room");
+var area = length * width;
+
+
