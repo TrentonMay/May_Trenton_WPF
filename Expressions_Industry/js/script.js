da37d2907b1acc3aff790efac2e27bf6c042b859
TrentonMay <TrentonMay@fullsail.edu>
2014-12-03 18:21:23 -0500

    Added comment
    //Tells user to enter width of the building

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -2,7 +2,7 @@
 //How many scan points on a foot
 
 alert ("We are calculating the amount of scan points you will scan");//Tells user what we are doing
-var width = prompt ("Enter the width of the building");
+var width = prompt ("Enter the width of the building");//Tells user to enter width of the building
 var length = prompt ('Enter the length of the building');
 var perimeter = width * 2 + length * 2;
 var freq = prompt ("Enter how often there is a scan points \n in feet");
