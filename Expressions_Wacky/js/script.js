fd419cb06437a03c1ea33f85947c55ab8df8b85b
TrentonMay <TrentonMay@fullsail.edu>
2014-12-03 18:55:30 -0500

    Added comment
    //Tells customer to enter length of pool

--- Expressions_Wacky/js/script.js
+++ Expressions_Wacky/js/script.js
@@ -5,9 +5,17 @@ alert ("We are calculating the amount of water need in your square pool \n Pleas
 //Telling user what we are doing
 
 var length = prompt ("Please enter the length of the pool in feet");
+//Tells customer to enter length of pool
+
 var width = prompt ("Please enter the width of the pool in feet");
+
+
 var depth = prompt ("Please enter the depth of the pool in feet");
+
+
 var volume = length * width * depth;
+
+
 var totalGal = volume * 7.47;
 
 alert ("You will need " + totalGal.toFixed(2) + " gallons to fill this pool ");
