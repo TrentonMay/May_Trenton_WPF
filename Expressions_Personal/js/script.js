c9db08e23e8aa59fe32058e30b685cfad8ac58ae
TrentonMay <TrentonMay@fullsail.edu>
2014-12-03 17:15:55 -0500

    Added comment

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -2,7 +2,7 @@
 //How much carpet is needed in the living room
 
 alert("We are calculating the amount of carpet needed in the living room");//We are telling the user what we are doing
-var width = prompt("Enter the width of the living room");
+var width = prompt("Enter the width of the living room");//Tells user to enter the  width of their living room
 var length = prompt("Enter the length of the living room");
 var area = length * width;
 
