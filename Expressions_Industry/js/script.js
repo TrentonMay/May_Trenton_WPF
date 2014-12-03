ed8fac9767ee403ebd33090031a0564cb333c0b6
TrentonMay <TrentonMay@fullsail.edu>
2014-12-03 18:22:57 -0500

    Added comment
    //Calculates perimeter

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -4,7 +4,7 @@
 alert ("We are calculating the amount of scan points you will scan");//Tells user what we are doing
 var width = prompt ("Enter the width of the building");//Tells user to enter width of the building
 var length = prompt ('Enter the length of the building');//Tells user to enter length of the building
-var perimeter = width * 2 + length * 2;
+var perimeter = width * 2 + length * 2;//Calculates perimeter
 var freq = prompt ("Enter how often there is a scan points \n in feet");
 var totalScan = perimeter / freq;
 
