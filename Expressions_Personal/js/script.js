8271731b05136089dd36d7750fc3390792abfc3e
TrentonMay <TrentonMay@fullsail.edu>
2014-12-03 17:36:42 -0500

    Added new variable

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -1,10 +1,11 @@
 //Expressions Personal
 //How much gas you will need
 
-alert("We are calculating the amount of gas you will need");//We are telling the user what we are doing
+alert("We are calculating the price of the amount of gas you will need");//We are telling the user what we are doing
 var ppg = prompt("Enter the price per gallon");//Tells user to enter the  price per gallon
 var mpg = prompt("Enter the miles per gallon your vehicle gets");//Tells user to enter their mpg
 var miles = prompt("Enter the amount of miles you will travel");//Tells user to enter how many miles they wish to travel
-var area = length * width;
+var gallons = miles / mpg;
+var total = gallons * ppg;
 
-alert("The amount of carpet you need is " + area + " square ft");
+alert("You will use this many gallons " + area + " square ft");
