e621861b503d7b4ac19d67f643bdd06238553fa9
TrentonMay <TrentonMay@fullsail.edu>
2014-12-03 18:24:26 -0500

    Added comment
    //Calculates how many scan points they have

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -6,7 +6,7 @@ var width = prompt ("Enter the width of the building");//Tells user to enter wid
 var length = prompt ('Enter the length of the building');//Tells user to enter length of the building
 var perimeter = width * 2 + length * 2;//Calculates perimeter
 var freq = prompt ("Enter how often there is a scan points \n in feet");//Tells user to enter how often there is a scan point
-var totalScan = perimeter / freq;
+var totalScan = perimeter / freq;//Calculates how many scan points they have
 
 alert ("The number of scan point you will have is " + Math.floor(totalScan));
 
