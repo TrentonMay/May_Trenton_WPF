faa4bcefcf29ddf5b2a1a4229012eb82a47e990d
TrentonMay <TrentonMay@fullsail.edu>
2014-12-03 05:05:07 -0500

    Added alert

--- ScreenCasts/Prompt and alert/js/script.js
+++ ScreenCasts/Prompt and alert/js/script.js
@@ -3,4 +3,6 @@
 var width = prompt("We are calculating the area of a rectangle. \n Please enter the width");
 var height = prompt("Please enter the height");
 var area  = width * height;
-console.log(area);
+//console.log(area);
+
+alert("The area of your rectangle is");
