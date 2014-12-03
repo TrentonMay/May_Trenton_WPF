c9577775511b82749b85e2d8a71f1f399f4973db
TrentonMay <TrentonMay@fullsail.edu>
2014-12-03 17:37:49 -0500

    Added new comment

--- Expressions_Personal/js/script.js
+++ Expressions_Personal/js/script.js
@@ -5,7 +5,7 @@ alert("We are calculating the price of the amount of gas you will need");//We ar
 var ppg = prompt("Enter the price per gallon");//Tells user to enter the  price per gallon
 var mpg = prompt("Enter the miles per gallon your vehicle gets");//Tells user to enter their mpg
 var miles = prompt("Enter the amount of miles you will travel");//Tells user to enter how many miles they wish to travel
-var gallons = miles / mpg;
+var gallons = miles / mpg;//This will calculate how many gallons you will need
 var total = gallons * ppg;
 
 alert("You will use this many gallons " + area + " square ft");
