97ea5891436a649e2fdbc340810a07683301e6d2
TrentonMay <TrentonMay@fullsail.edu>
2014-12-04 02:14:49 -0500

    Fixed some text misspellings

--- Expressions_Worksheet/Dog Years/js/script.js
+++ Expressions_Worksheet/Dog Years/js/script.js
@@ -3,5 +3,5 @@
 var humanYears = 5;
 var dogYears = humanYears * 7;
 
-console.log("Sparky is " + humanYears + " years old which is" + dogYears+ " in dog years" )
+console.log("Sparky is " + humanYears + " years old which is " + dogYears+ " in dog years" )
 
