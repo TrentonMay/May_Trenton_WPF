9921ea88b237a345345e20c85c751750d38998fd
TrentonMay <TrentonMay@fullsail.edu>
2014-12-04 03:19:34 -0500

    Fixed misspelling

--- Expressions_Worksheet/Discounts/js/script.js
+++ Expressions_Worksheet/Discounts/js/script.js
@@ -11,6 +11,6 @@ var st = salesTax / 100;//sales tax
 var dp = op - (op * dc);//discount price
 var total = dp + (dp * st);
 
-console.log("Your " + description + " was originally " + "$" + op + " , but after a " + discount +"%" + " discount, it is now " + "$" + dp + " without tax, and " + "$" + total + " with tax");
+console.log("Your " + description + " was originally " + "$" + op + ", but after a " + discount +"%" + " discount, it is now " + "$" + dp + " without tax, and " + "$" + total + " with tax");
 
 
