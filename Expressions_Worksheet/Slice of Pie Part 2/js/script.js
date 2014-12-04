65b3235c57f9ffe6985e459096c0fef01ed042f2
TrentonMay <TrentonMay@fullsail.edu>
2014-12-04 02:36:28 -0500

    Wrote expression

--- Expressions_Worksheet/Slice of Pie Part 2/js/script.js
+++ Expressions_Worksheet/Slice of Pie Part 2/js/script.js
@@ -5,4 +5,9 @@ var people = 20;
 var pizzas = 6;
 
 var totalSlices = slices * pizzas;
-var spp = totalSlices / people;
+var spp = Math.floor(totalSlices / people);
+
+var leftOvers = totalSlices - (spp * people);
+
+console.log("Sparky got " + Math.floor(leftOvers) + " pieces of pizza");
+
