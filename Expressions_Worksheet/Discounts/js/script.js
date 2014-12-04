cadca00df68a6b88840bad22d7f85a29824c615b
TrentonMay <TrentonMay@fullsail.edu>
2014-12-04 03:12:54 -0500

    Created var for total

--- Expressions_Worksheet/Discounts/js/script.js
+++ Expressions_Worksheet/Discounts/js/script.js
@@ -2,9 +2,15 @@
 
 var op = 30;
 var discount = 25;
-var decription = "Milk";
+var description = "Milk";
 var salesTax = 5;
 
-var dp = discount / 100;//discount percentage
+var dc = discount / 100;//discount percentage
 var st = salesTax / 100;//sales tax
+
+var dp = op - (op * dc);//discount price
+var total = dp + (dp * st);
+
+
+
 
