410cbf5f3392c37564833a6b56d5a924546ca7a8
TrentonMay <TrentonMay@fullsail.edu>
2014-12-03 19:21:54 -0500

    Cleaned up the script

--- Expressions_Industry/js/script.js
+++ Expressions_Industry/js/script.js
@@ -7,13 +7,8 @@ secure [0] = prompt ("Enter the width of the building");//Tells user to enter wi
 secure [1] = prompt ("Enter the length of the building");//Tells user to enter length of the building
 secure [2] = prompt ("Enter how often there is a scan points \n in feet");//Tells user to enter how often there is a scan point
 
-secure [3] = secure[0] * 2 + secure[1] * 2;
-secure [4] = secure[3] / secure[2];
-//var width = prompt ("Enter the width of the building");//Tells user to enter width of the building
-//var length = prompt ('Enter the length of the building');//Tells user to enter length of the building
-//var perimeter = width * 2 + length * 2;//Calculates perimeter
-//var freq = prompt ("Enter how often there is a scan points \n in feet");//Tells user to enter how often there is a scan point
-//var totalScan = perimeter / freq;//Calculates how many scan points they have
+secure [3] = secure[0] * 2 + secure[1] * 2;//Calculates perimeter
+secure [4] = secure[3] / secure[2];//Calculates how many scan points they have
 
 alert ("The number of scan points you will have is " + Math.floor(secure[4]));
 //Tells user the end result
