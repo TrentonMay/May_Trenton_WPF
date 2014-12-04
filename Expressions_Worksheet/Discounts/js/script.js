bebf5550585c06709cd4193d5dbd5785c475f73d
TrentonMay <TrentonMay@fullsail.edu>
2014-12-04 03:09:03 -0500

    Began expression

--- Expressions_Worksheet/Discounts/js/script.js
+++ Expressions_Worksheet/Discounts/js/script.js
@@ -1,3 +1,10 @@
-/**
- * Created by Trent on 12/4/14.
- */
+//Discounts
+
+var op = 30;
+var discount = 25;
+var decription = "Milk";
+var salesTax = 5;
+
+var dp = discount / 100;//discount percentage
+var st = salesTax / 100;//sales tax
+
