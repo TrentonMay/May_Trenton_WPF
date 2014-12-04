60ba66226073376c243f16d6c49a6bd8d5e44a4d
TrentonMay <TrentonMay@fullsail.edu>
2014-12-04 02:55:49 -0500

    Limited decimals

--- Expressions_Worksheet/Average Shopping bill/js/script.js
+++ Expressions_Worksheet/Average Shopping bill/js/script.js
@@ -4,5 +4,5 @@ var grocTotal = [30.25, 70.57, 24.89, 56, 43.67];
 var total = grocTotal[0] + grocTotal[1] + grocTotal[2] + grocTotal[3] + grocTotal[4];
 var average = total / 5;
 
-console.log("You have spent a total of " + "$" + total + " on groceries over 5 weeks.\n That is an average of "+ "$" + average.toFixed(2) + " per week" );
+console.log("You have spent a total of " + "$" + total.toFixed(2) + " on groceries over 5 weeks.\n That is an average of "+ "$" + average.toFixed(2) + " per week" );
 
