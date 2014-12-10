e078c713be16fffff88bd0f589d83296888c4ef8
TrentonMay <TrentonMay@fullsail.edu>
2014-12-10 17:01:22 -0500

    Added alert

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -1,9 +1,11 @@
 //Industrial conditional
 //This script will tell you if you can drink something
 
+alert("We will help you decide if you can drink your beverage legally");
 var age = prompt("How old are you?");
 var isAlcoholic = prompt("Is the drink alcoholic");
 var legalAge = 21;
+
 if (age == "" || isAlcoholic == "") {
     alert("You did not enter the required information. Please refresh the page.");
 } else {
