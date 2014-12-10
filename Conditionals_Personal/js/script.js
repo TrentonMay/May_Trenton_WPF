e788a070b69ce1b4f3739612cd6b08c8cae26c1d
TrentonMay <TrentonMay@fullsail.edu>
2014-12-10 16:01:25 -0500

    Created variable

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -1,3 +1,4 @@
 //Conditionals Personal
 //Vaping. If you have a RDA then you will need more liquid because you are burning more liquid. If you have a  RBA you will burn less liquid
 
+var mod = prompt ("Do you have a RDA or a RBA?");
