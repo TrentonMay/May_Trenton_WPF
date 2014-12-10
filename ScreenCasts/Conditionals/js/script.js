cd2162d7bdd7447f07f6f1e55f6954425f2e3946
TrentonMay <TrentonMay@fullsail.edu>
2014-12-09 19:06:08 -0500

    Completed and understood fifth screencast

--- ScreenCasts/Conditionals/js/script.js
+++ ScreenCasts/Conditionals/js/script.js
@@ -1,11 +1,14 @@
 //Conditionals
 
-var kidHeight = 49;
+var kidHeight = 54;
 var minHeight = 48;
+var parentHeight = 45;
 
 if(kidHeight > minHeight){
     console.log("you can ride the coaster!");
 
+}else if(kidHeight > parentHeight){
+    console.log("You may ride, but only with a parent present");
 }else{
     console.log("Sorry kid, you've got some growing to do.");
 }
