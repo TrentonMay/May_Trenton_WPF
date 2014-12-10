34ce99d2b0b0887f7661133798da121cb18856a4
TrentonMay <TrentonMay@fullsail.edu>
2014-12-10 16:33:36 -0500

    Created new variable

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -1,4 +1,5 @@
 //Industrial conditional
 //This script will tell you if you need to work overtime
 
-var workers = prompt("How many workers do you have?"); 
+var workers = prompt("How many workers do you have?");
+var partsNeeded = prompt("How many parts do you need?");
