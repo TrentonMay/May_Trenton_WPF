9735968cb9618abb96c48397e8ea477ee9a51959
TrentonMay <TrentonMay@fullsail.edu>
2014-12-10 16:53:05 -0500

    Started if statement

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -8,5 +8,5 @@ var legalAge = 21;
 if (age == "" || isAlcoholic == ""){
     alert("You did not enter the required information. Please refresh the page.");
 }else{
-    
+    if (isAlcoholic == "yes") {
 }
