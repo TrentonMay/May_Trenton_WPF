4e3cab9c102cd8acc6649646c8b4564aec5ee13b
TrentonMay <TrentonMay@fullsail.edu>
2014-12-09 19:33:11 -0500

    Completed screencast

--- ScreenCasts/Conditionals/js/script.js
+++ ScreenCasts/Conditionals/js/script.js
@@ -1,2 +1,12 @@
 //Conditionals
 
+var budget = 300;
+var iPhonePrice = 199.99;
+var paycheck = 200;
+var wonLottery = true;
+
+if(iPhonePrice < budget || wonLottery){
+    console.log("We can buy the phone!");
+}else{
+    console.log("No phone for you!!");
+}
