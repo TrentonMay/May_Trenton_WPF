49b8cf252cf761746d814be5d3b158c73b36eeb6
TrentonMay <TrentonMay@fullsail.edu>
2014-12-10 16:35:43 -0500

    Created new variable

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -5,3 +5,4 @@ var workers = prompt("How many workers do you have?");
 var partsNeeded = prompt("How many parts do you need?");
 var workday = 8; //how long in hours an average work day is
 var ppm = 1; //how many parts per minute a single employee can do
+var ppd = workers * ppm * workday;
