daca98000307c89b3c93b2059e06f570a353e0ad
TrentonMay <TrentonMay@fullsail.edu>
2014-12-10 16:00:57 -0500

    Created comments
    //Vaping. If you have a RDA then you will need more liquid because you
    are burning more liquid. If you have a  RBA you will burn less liquid

--- Conditionals_Personal/js/script.js
+++ Conditionals_Personal/js/script.js
@@ -1,3 +1,3 @@
-/**
- * Created by Trent on 12/10/14.
- */
+//Conditionals Personal
+//Vaping. If you have a RDA then you will need more liquid because you are burning more liquid. If you have a  RBA you will burn less liquid
+
