5eb737d57238f9d61de9117e5c5908914e510b2e
TrentonMay <TrentonMay@fullsail.edu>
2014-12-10 16:36:03 -0500

    Wrote new comment
    //how many parts all the workers can make an a workday

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -5,4 +5,4 @@ var workers = prompt("How many workers do you have?");
 var partsNeeded = prompt("How many parts do you need?");
 var workday = 8; //how long in hours an average work day is
 var ppm = 1; //how many parts per minute a single employee can do
-var ppd = workers * ppm * workday;
+var ppd = workers * ppm * workday; //how many parts all the workers can make an a workday
