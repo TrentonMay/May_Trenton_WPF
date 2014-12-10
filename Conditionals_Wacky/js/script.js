cc651344268927ccc62818e7e171f47c24227d9d
TrentonMay <TrentonMay@fullsail.edu>
2014-12-10 16:56:51 -0500

    Code sanitation

--- Conditionals_Wacky/js/script.js
+++ Conditionals_Wacky/js/script.js
@@ -4,19 +4,16 @@
 var age = prompt("How old are you?");
 var isAlcoholic = prompt("Is the drink alcoholic");
 var legalAge = 21;
-
-if (age == "" || isAlcoholic == ""){
+if (age == "" || isAlcoholic == "") {
     alert("You did not enter the required information. Please refresh the page.");
-}else{
-    if (isAlcoholic == "yes"){
+} else {
+    if (isAlcoholic == "yes") {
         if (age >= legalAge) {
             alert("Drink up!");
-        }else{
+        } else {
             alert("Can't touch this");
         }
-
-        }else{
+    } else {
         alert("Drink up!");
     }
-
 }
