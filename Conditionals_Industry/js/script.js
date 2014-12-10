4da28173c58aeff5b00b0b87df26245fcdb38145
TrentonMay <TrentonMay@fullsail.edu>
2014-12-10 16:36:49 -0500

    Wrote new comment
    //now we will make sure they answered the questions with our first if
    statement

--- Conditionals_Industry/js/script.js
+++ Conditionals_Industry/js/script.js
@@ -6,3 +6,5 @@ var partsNeeded = prompt("How many parts do you need?");
 var workday = 8; //how long in hours an average work day is
 var ppm = 1; //how many parts per minute a single employee can do
 var ppd = workers * ppm * workday; //how many parts all the workers can make an a workday
+
+//now we will make sure they answered the questions with our first if statement
