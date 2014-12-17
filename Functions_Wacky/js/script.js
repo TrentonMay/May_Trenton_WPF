36c9b1962f6052d3aaf0b55abc7aef666a8e4067
TrentonMay <TrentonMay@fullsail.edu>
2014-12-17 01:10:24 -0500

    Added comment
    //Keep in mind I live in virginia so this is how bad our roads are with
    slight snow!

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -1,6 +1,7 @@
 //Trenton May, Scalable Data Infrastructures 16/12/14
 //Function_Wacky
 //Can you drive in the snow, or do you need to drive cautiously
+//Keep in mind I live in virginia so this is how bad our roads are with slight snow!
 
 function canDrive(snow){
     if(snow == 0){
