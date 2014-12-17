d0cc45448b6b9cd7374bc329a95c1422c1935214
TrentonMay <TrentonMay@fullsail.edu>
2014-12-16 20:30:57 -0500

    Completed returning values screencast

--- ScreenCasts/Functions/js/script.js
+++ ScreenCasts/Functions/js/script.js
@@ -1,8 +1,10 @@
 //Functions Screencasts
 
-function dogYears(age){
-    var dogYears = age * 7;
-    console.log("Sparky is " + dogYears + " years old")
+var total = calcArea(30, 20);
+
+function calcArea(w, h){
+    var area = w * h;
+    return area;
 }
 
-dogYears(4);
+console.log(total);
