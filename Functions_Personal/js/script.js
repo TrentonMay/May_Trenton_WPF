e640fe33f91bb552726e2cf16d1d7922e10a19b5
TrentonMay <TrentonMay@fullsail.edu>
2014-12-17 00:47:31 -0500

    Placed a console.log

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -1,9 +1,10 @@
 //Function for Personal
 //How many fish can you have in your tank
-//Two inch of fish per gallon
+//Two inches of fish per gallon
 
 function tankVol(length, width, height, fishInches){
     hmInches = length * width * height * 7.47 / 2;
+    console.log(hmInches / fishInches);
 }
 
 tankVol(3, 5, 7, 10);
