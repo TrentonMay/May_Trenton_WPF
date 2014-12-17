b4aa8f4630adad66e4987121627e8d047c34bf04
TrentonMay <TrentonMay@fullsail.edu>
2014-12-17 01:09:37 -0500

    Created if statement and else if statement

--- Functions_Wacky/js/script.js
+++ Functions_Wacky/js/script.js
@@ -3,6 +3,11 @@
 //Can you drive in the snow, or do you need to drive cautiously
 
 function canDrive(snow){
-    
+    if(snow == 0){
+        console.log("You can drive!");
+    }else if(snow > 0 && snow <=3){
+
+    }
+
 
 }
