207361aacd2784fbaa107fde70f8882eedd46d25
TrentonMay <TrentonMay@fullsail.edu>
2014-12-16 20:05:27 -0500

    Finished basic functions screencast

--- ScreenCasts/Functions/js/script.js
+++ ScreenCasts/Functions/js/script.js
@@ -1,2 +1,12 @@
 //Functions Screencasts
 
+function outptMsg(){
+    console.log("Hello World");
+}
+
+function calcArea(){
+    var width = 20;
+    var height = 30;
+    var area = width * height;
+    console.log(area);
+}
