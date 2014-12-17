5b2f174a427d8db265c86b85831cebd579e0443b
TrentonMay <TrentonMay@fullsail.edu>
2014-12-17 00:23:11 -0500

    Added comment
    //One guppy per gallon

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -1,11 +1,14 @@
 //Function for Personal
-//How many fish can you have in your tank
+//How many guppies can you have in your tank
+//One guppy per gallon
 
 function tankVol(length, width, height){
     return length * width * height;
 }
 
 gallons = tankVol(30, 40, 67) * 7.47;
+
+
 
 
 
