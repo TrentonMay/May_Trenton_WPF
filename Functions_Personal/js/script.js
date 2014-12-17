90747e17a65016de20714d91b0ccfc78335f7cc4
TrentonMay <TrentonMay@fullsail.edu>
2014-12-17 00:51:27 -0500

    Edited console.log

--- Functions_Personal/js/script.js
+++ Functions_Personal/js/script.js
@@ -4,7 +4,7 @@
 
 function tankVol(length, width, height, fishInches){ //fishInches = average amount of inches you have per fish
     hmInches = length * width * height * 7.47 / 2;//Here we calculate how many inches of fish you can have
-    console.log(hmInches / fishInches);//This prints out how many fish you can place in the tank
+    console.log("You may have " + hmInches / fishInches + " Fish");//This prints out how many fish you can place in the tank
 }
 
 tankVol(3, 5, 7, 10);
