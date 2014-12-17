f10b4e8c924a7bf1f61db232d43dcfe94dc201ab
TrentonMay <TrentonMay@fullsail.edu>
2014-12-16 23:33:57 -0500

    Added function

--- Functions_Industry/js/script.js
+++ Functions_Industry/js/script.js
@@ -2,4 +2,14 @@
 //Calculating how many buns Arby's needs to order for their sandwiches
 
 
+var checkBuns = function(buns){
+    if(buns >= 100){
+        return false;
+    }else{
+        return true;
+    }
+
+}
+
+
 
