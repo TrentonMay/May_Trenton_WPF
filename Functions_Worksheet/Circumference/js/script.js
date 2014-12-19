ef886f74eaff6a87af80be29a83345ab7cab35f8
TrentonMay <TrentonMay@fullsail.edu>
2014-12-18 20:47:16 -0500

    Added comment
    //This names the function and sets up the formula inside

--- Functions_Worksheet/Circumference/js/script.js
+++ Functions_Worksheet/Circumference/js/script.js
@@ -5,7 +5,7 @@ var circumference = calcCirc(radius * 2);//This is where I'm calling on the func
 
 console.log("The circumference of your circle is " + circumference.toFixed(2));//This prints out the result to the console. I also limited the decimals
 
-function calcCirc(diameter){
+function calcCirc(diameter){ //This names the function and sets up the formula inside
     return diameter * 3.14;
 }
 
