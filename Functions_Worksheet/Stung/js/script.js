09d1613e08e270cf322cd6a0cc051fdc08e3b9e1
TrentonMay <TrentonMay@fullsail.edu>
2014-12-18 20:50:30 -0500

    Added comment
    //Tells the function to return weight

--- Functions_Worksheet/Stung/js/script.js
+++ Functions_Worksheet/Stung/js/script.js
@@ -5,7 +5,7 @@ var kill = beeKill(45); //calling on the function here
 console.log("It takes " + kill.toFixed(2) + " stings to kill this animal"); // this is printing the result to the console. I also limited the decimals
 
 function beeKill(weight){ //sets up the function to contain the present formula
-    return weight * 8.666666667;
+    return weight * 8.666666667; //Tells the function to return weight
 
 }
 
