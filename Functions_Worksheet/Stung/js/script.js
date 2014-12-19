831460c7809e0208d76bac859c94e7d48a7a677f
TrentonMay <TrentonMay@fullsail.edu>
2014-12-18 20:49:25 -0500

    Added comment
    // this is printing the result to the console. I also limited the
    decimals

--- Functions_Worksheet/Stung/js/script.js
+++ Functions_Worksheet/Stung/js/script.js
@@ -2,7 +2,7 @@
 
 
 var kill = beeKill(45); //calling on the function here
-console.log("It takes " + kill.toFixed(2) + " stings to kill this animal");
+console.log("It takes " + kill.toFixed(2) + " stings to kill this animal"); // this is printing the result to the console. I also limited the decimals
 
 function beeKill(weight){
     return weight * 8.666666667;
