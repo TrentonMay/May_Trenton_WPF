4b9a6707c181e25a5ecbac4408ceb8f2365f71c8
TrentonMay <TrentonMay@fullsail.edu>
2014-12-18 20:50:01 -0500

    Added comment
    //sets up the function to contain the present formula

--- Functions_Worksheet/Stung/js/script.js
+++ Functions_Worksheet/Stung/js/script.js
@@ -4,7 +4,7 @@
 var kill = beeKill(45); //calling on the function here
 console.log("It takes " + kill.toFixed(2) + " stings to kill this animal"); // this is printing the result to the console. I also limited the decimals
 
-function beeKill(weight){
+function beeKill(weight){ //sets up the function to contain the present formula
     return weight * 8.666666667;
 
 }
