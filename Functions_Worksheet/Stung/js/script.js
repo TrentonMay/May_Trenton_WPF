056b8b95571fcf168ee48cacc91d7fcd1f5f20c2
TrentonMay <TrentonMay@fullsail.edu>
2014-12-18 20:48:51 -0500

    Added comment to Stung
    //calling on the function here

--- Functions_Worksheet/Stung/js/script.js
+++ Functions_Worksheet/Stung/js/script.js
@@ -1,7 +1,7 @@
 //Stung
 
 
-var kill = beeKill(45);
+var kill = beeKill(45); //calling on the function here
 console.log("It takes " + kill.toFixed(2) + " stings to kill this animal");
 
 function beeKill(weight){
