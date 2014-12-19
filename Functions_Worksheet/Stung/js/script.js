cc1fa07ac62fce0beb6413438fbce209d9acd504
TrentonMay <TrentonMay@fullsail.edu>
2014-12-18 20:24:29 -0500

    Made changes to Circumference
    Fixed it to go with parameters of the assignment

--- Functions_Worksheet/Stung/js/script.js
+++ Functions_Worksheet/Stung/js/script.js
@@ -1,12 +1,17 @@
 //Stung
 
+var weight = 45;
+var death = weight * 8.666666667;
+var kill = beeKill(45);
+console.log("It takes " + beeKill(45).toFixed(2) + " stings to kill this animal");
+
 function beeKill(weight){
     death = weight * 8.666666667;
     return death;
 
 }
 
-console.log("It takes " + beeKill(45).toFixed(2) + " stings to kill this animal");
+
 
 
 
