db8279a05b59b40688041426149443fb3015068e
TrentonMay <TrentonMay@fullsail.edu>
2014-12-18 20:36:09 -0500

    Changed code in circumference
    Needed to fit assignment parameters

--- Functions_Worksheet/Circumference/js/script.js
+++ Functions_Worksheet/Circumference/js/script.js
@@ -1,5 +1,10 @@
 //Circumference
 
+var radius = ;
+var diameter = radius * 2;
+
+console.log("The circumference of your circle is " + cirumference.toFixed(2));
+
 function calcCirc(radius){
     circ = (radius * 2) * 3.14;
     return circ;
