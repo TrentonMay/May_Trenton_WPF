6973c734817e73c994e18ce6bef9b2c024e5d643
TrentonMay <TrentonMay@fullsail.edu>
2014-12-18 20:37:06 -0500

    Added variable

--- Functions_Worksheet/Circumference/js/script.js
+++ Functions_Worksheet/Circumference/js/script.js
@@ -1,11 +1,12 @@
 //Circumference
 
-var radius = ;
+var radius = 4;
 var diameter = radius * 2;
+var circumference = calcCirc();
 
 console.log("The circumference of your circle is " + cirumference.toFixed(2));
 
-function calcCirc(radius){
+function calcCirc(){
     circ = (radius * 2) * 3.14;
     return circ;
 }
