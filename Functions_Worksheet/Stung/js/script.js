042cf02ec70c4750577c734a9bf935265ba6cd56
TrentonMay <TrentonMay@fullsail.edu>
2014-12-18 20:08:32 -0500

    Completed Stung function
    It worked on my first try. WOO!

--- Functions_Worksheet/Stung/js/script.js
+++ Functions_Worksheet/Stung/js/script.js
@@ -1,4 +1,12 @@
-//Circumference
+//Stung
+
+function beeKill(weight){
+    death = weight * 8.666666667;
+    return death;
+
+}
+
+console.log("It takes " + beeKill(45).toFixed(2) + " stings to kill this animal");
 
 
 
