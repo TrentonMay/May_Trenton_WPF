038859960b77b4718f0d8588722b9f6da3a475af
TrentonMay <TrentonMay@fullsail.edu>
2014-12-18 20:24:49 -0500

    Saved file

--- Functions_Worksheet/Stung/js/script.js
+++ Functions_Worksheet/Stung/js/script.js
@@ -6,7 +6,6 @@ var kill = beeKill(45);
 console.log("It takes " + beeKill(45).toFixed(2) + " stings to kill this animal");
 
 function beeKill(weight){
-    death = weight * 8.666666667;
     return death;
 
 }
